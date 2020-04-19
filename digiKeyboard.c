9d7ac2f86b33b2d1cb191e75268fd7aceadf0a45
Yusuke Kurokawa <wotakuro@gmail.com>
2020-04-19 13:58:29 +0900

    update

--- digiKeyboard.c
+++ digiKeyboard.c
@@ -1,31 +1,47 @@
 // ※押している間、Unity側では常にInput.GetKeyDown/GetKeyUpが trueになります。
 // 処理には、1フレーム分バッファして工夫するなどが必要です。 
-
 #include "DigiKeyboard.h"
+
+// ボタンを押した瞬間のみ処理したい場合はコチラ
+#define INPUT_DOWN_ONLY
+
 const int LED_PIN = 0;
 const int LED_PIN2 = 1;
 const int BTN_PIN = 2;
 
 void setup() {
-  // don't need to set anything up to use DigiKeyboard
-  pinMode(LED_PIN,OUTPUT);  
-  pinMode(LED_PIN2,OUTPUT);  
-  pinMode(BTN_PIN,INPUT);
-  DigiKeyboard.sendKeyStroke(0);  
+    // don't need to set anything up to use DigiKeyboard
+    pinMode(LED_PIN, OUTPUT);
+    pinMode(LED_PIN2, OUTPUT);
+    pinMode(BTN_PIN, INPUT);
+    DigiKeyboard.sendKeyStroke(0);
 }
+#ifdef INPUT_DOWN_ONLY
+int previewVal = HIGH;
+#endif
 
 void loop() {
-  int val = 0;
+    int val = 0;
 
-  val = digitalRead( BTN_PIN);
-  if( val != HIGH  ){
-      DigiKeyboard.sendKeyStroke(KEY_ENTER);
-      digitalWrite(LED_PIN , HIGH);
-      digitalWrite(LED_PIN2 , HIGH);
-  }else{
-      digitalWrite(LED_PIN , LOW);
-      digitalWrite(LED_PIN2 , LOW);
-      DigiKeyboard.delay(8);
-  }
-  DigiKeyboard.delay(2);
+    val = digitalRead(BTN_PIN);
+    if (val != HIGH) {
+#ifdef  INPUT_DOWN_ONLY
+        if (previewVal != val) {
+            DigiKeyboard.sendKeyStroke(KEY_ENTER);
+        }
+#else
+        DigiKeyboard.sendKeyStroke(KEY_ENTER);
+#endif
+        digitalWrite(LED_PIN, HIGH);
+        digitalWrite(LED_PIN2, HIGH);
+    }
+    else {
+        digitalWrite(LED_PIN, LOW);
+        digitalWrite(LED_PIN2, LOW);
+        DigiKeyboard.delay(8);
+    }
+#ifdef INPUT_DOWN_ONLY
+    previewVal = val;
+#endif
+    DigiKeyboard.delay(2);
 }
